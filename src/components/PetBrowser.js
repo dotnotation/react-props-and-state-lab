9b7a2d82b7b516026b45bb0c104f055bc1309d21
dotnotation <dotinmotionproductions@gmail.com>
2022-02-01 19:09:55 -0500

    commit tree 4f368f272506095c782bc7939cab642169f2d5a7

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,18 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  petCards = () => {
+    return this.props.pets.map(petAttributes => {
+      return <Pet key={petAttributes.id} pet={petAttributes} onAdoptPet={this.props.onAdoptPet} />
+    })
+  }
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+      <div className="ui cards">
+        {this.petCards()}
+      </div>
+    )
   }
 }
 
